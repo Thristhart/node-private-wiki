33131e1ed56b258495551ca0f8224bb8e64742e5
Tom Shea <tom@shea.at>
2016-07-16 02:04:50 -0400

    Initial index.js with https upgrade and 'hello world' koa server

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const config = require('./config.json');
+const fs = require('fs');
+
+const http = require('http');
+const https = require('https');
+
+let httpsOptions = {
+  key: fs.readFileSync(config.keyPath, 'ascii'),
+  cert: fs.readFileSync(config.certPath, 'ascii'),
+};
+
+const koa = require('koa');
+const enforceHttps = require('koa-sslify');
+
+const app = koa();
+
+// Force HTTPS on all page
+app.use(enforceHttps());
+
+app.use(function *() {
+  this.body = 'Hello World';
+});
+
+
+// start the server
+http.createServer(app.callback()).listen(80);
+https.createServer(httpsOptions, app.callback()).listen(443);
